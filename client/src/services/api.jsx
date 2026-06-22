b39c8650a225ae93ce3b535d8b3cf6a31582900e
Neha Vetal <nehavetal520@gmail.com>
2026-06-22 19:22:23 +0530

    fix: correct backend api baseURL with /api

--- client/src/services/api.jsx
+++ client/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://poojaconnect-fz3b.onrender.com"
+  baseURL: "https://poojaconnect-fz3b.onrender.com/api"
 });
 
 api.interceptors.request.use((config) => {
